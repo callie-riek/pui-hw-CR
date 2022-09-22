8cce6aa1cd44e0a151368c7dd5ffa411d026cf74
callie-riek <callie.riek@gmail.com>
2022-09-22 16:56:13 -0400

    changed array

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -1,5 +1,3 @@
-console.log("howdy")
-
 var basePrice = 2.49;
 var glazingPrice = 0;
 var packAdapt = 1; //setting packAdapt to 1 because if I set it to 0 it will calculate
@@ -7,27 +5,18 @@ var packAdapt = 1; //setting packAdapt to 1 because if I set it to 0 it will cal
 
 //calculate new price
 function changePrice() {
-    var newPrice = ((basePrice + glazingPrice) * packAdapt).toFixed(2)
-    console.log("new price", newPrice)
-    document.getElementById("price").innerHTML = "$" + newPrice
-    console.log("price in change price function",newPrice)
+    var newPrice = ((basePrice + glazingPrice) * packAdapt).toFixed(2);
+    document.getElementById("price").innerHTML = "$" + newPrice;
 }
 
-
 ///////////////GLAZE OPTIONS////////////////////
-
-function Glaze(type, price) {
-    this.type = type;
-    this.price = price;
-} //create a class with constructor inside the class
-
-const original = new Glaze('Keep original', 0);
-const SugarMilk = new Glaze('Sugar Milk', 0);
-const VanillaMilk = new Glaze('Vanilla Milk', 0.5);
-const DoubleChocolate = new Glaze('Double Chocolate', 1.5);
-
-const GlazeOptionsArray = [ original, SugarMilk, VanillaMilk, DoubleChocolate];
-console.log("glaze array",GlazeOptionsArray);
+//array contains all the elements needed in the dropdown
+const GlazeOptionsArray = [
+    {type: "Keep original", price: 0},
+    {type:"Sugar Milk", price: 0},
+    {type: "Vanilla Milk", price: 0.5},
+    {type: "Double Chocolate", price: 1.5}
+];
 
 let glaze = document.getElementById("glazingOptions");
 
@@ -39,37 +28,28 @@ for (let i=0; i < GlazeOptionsArray.length; i++){
 
 //////////////////PACK SIZE ///////////////////////
 
-function PackSize(size, adapt) {
-    this.size = size;
-    this.adapt = adapt;
-}
-
-const One = new PackSize("1", 1);
-const Three = new PackSize("3", 3);
-const Six = new PackSize("6", 5);
-const Twelve = new PackSize("12", 10);
-
-const PackOptionsArray = [ One, Three, Six, Twelve];
-console.log("pack array",PackOptionsArray);
+const PackOptionsArray = [
+    {size: "1", adapt: 1},
+    {size:"3", adapt: 3},
+    {size: "5", adapt: 5},
+    {size: "12", adapt: 10}
+];
 
 let packSizeOptions = document.getElementById("packSizeOptions");
 
-//this function loops through the array to create new objects and add them
-// to the inner HTML
+//this function loops through the array to create new objects and add them to the inner HTML
 for (let i=0; i < PackOptionsArray.length; i++){
     let packOption = document.createElement("option");
     packOption.innerHTML = PackOptionsArray[i].size;
     packSizeOptions.appendChild(packOption);
 }
 
-//if the value of the element the dropdown selected matches something in the glaze
-//glaze options Array,the variable glazingPrice should be reset to the price paired with that type in the array 
+//if the value of the element the dropdown selected matches something in the glaze options Array,the variable glazingPrice should be reset to the price paired with that type in the array 
 function glazingChange(element) {
     for (let i=0; i < GlazeOptionsArray.length; i++){
         if (element.value === GlazeOptionsArray[i].type) {
             glazingPrice = GlazeOptionsArray[i].price; 
-        } 
-        console.log("glaze price", glazingPrice)        
+        }        
 }
 changePrice()
 }
@@ -80,7 +60,6 @@ function packChange(element) {
         if (element.value === PackOptionsArray[i].size) {
             packAdapt = PackOptionsArray[i].adapt; 
         } 
-        console.log("pack adapt", packAdapt)  
 }
 changePrice()
 }
