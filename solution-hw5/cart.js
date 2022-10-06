18ffd6ef28490da72441557c325b891290d290ad
callie-riek <callie.riek@gmail.com>
2022-10-06 19:49:57 -0400

    clean up JS

--- solution-hw5/cart.js
+++ solution-hw5/cart.js
@@ -19,10 +19,10 @@ let newBun3 = new Roll ("raisin", "Sugar Milk", 3, 2.99);
 let newBun4 = new Roll ("apple", "Keep Original", 3, 3.49);
 
 //add roll objects to cart set
-cartTwo.add(newBun1)
-cartTwo.add(newBun2)
-cartTwo.add(newBun3)
-cartTwo.add(newBun4)
+cartTwo.add(newBun1);
+cartTwo.add(newBun2);
+cartTwo.add(newBun3);
+cartTwo.add(newBun4);
 
 //arrays with values needed in price calculation
 //price adapt
@@ -31,7 +31,7 @@ let priceAdaption = {
     3:3,
     6:5,
     12:10
-}
+};
 
 //glaze
 let glazingPrice = {
@@ -39,7 +39,7 @@ let glazingPrice = {
     "Sugar Milk":0,
     "Vanilla Milk":0.5,
     "Double Chcolate":1.5
-}
+};
 
 //calculate price for individual items
 function calculatePrice(basePrice, glazing, packSize) {
@@ -50,7 +50,7 @@ function calculatePrice(basePrice, glazing, packSize) {
 function totalPrice() {
     let cartTotal = 0;
     for (let cartObject of cartTwo) {
-        finalPrice = calculatePrice(cartObject.basePrice, glazingPrice[cartObject.glazing], priceAdaption[cartObject.packSize])
+        finalPrice = calculatePrice(cartObject.basePrice, glazingPrice[cartObject.glazing], priceAdaption[cartObject.packSize]);
         cartTotal = finalPrice + cartTotal;
     }
 //update HTML with cart total
@@ -61,13 +61,13 @@ document.querySelector(".finalCartPrice").innerText = "$" + cartTotal.toFixed(2)
 //clone objects into HTML template
 function createCart(data) {
     for (let cartObject of cartTwo) {
-        let cartElement = document.getElementById("ItemOne")
+        let cartElement = document.getElementById("ItemOne");
         let docFragment = cartElement.content.cloneNode(true);
-        let clone = docFragment.querySelector('.cart-inner-row')
+        let clone = docFragment.querySelector('.cart-inner-row');
 
         //populate HTML with text/ images
-        clone.querySelector(".smaller-image").src = "assets/products/" + cartObject.type + "-cinnamon-roll.jpg"
-        clone.querySelector(".smaller-image").alt = cartObject.type + " " + "Cinnamon Roll"
+        clone.querySelector(".smaller-image").src = "assets/products/" + cartObject.type + "-cinnamon-roll.jpg";
+        clone.querySelector(".smaller-image").alt = cartObject.type + " " + "Cinnamon Roll";
         clone.querySelector(".rollName").innerText = cartObject.type + " " + "Cinnamon Roll";
         clone.querySelector(".glazeType").innerText = "Glazing:" + cartObject.glazing;
         clone.querySelector(".packSize").innerText = "Pack Size:" + cartObject.packSize;
@@ -82,7 +82,7 @@ function createCart(data) {
             clone.remove(); //remove HTML element
             cartTwo.delete(cartObject); //remove cart object from cart set
             totalPrice(); //call total price function here so it will run each time something is deleted   
-        })
+        });
 
         totalPrice(); //call total price for each object
         document.querySelector(".whole-cart").appendChild(clone); //append everything to whole cart  
