87d102376b05c26428491eeda44800ef3ed5a0a1
callie-riek <callie.riek@gmail.com>
2022-12-03 21:57:04 -0500

    added button row

--- Final project/js/interaction.js
+++ Final project/js/interaction.js
@@ -2,33 +2,60 @@
 
 var controller = new ScrollMagic.Controller(); //use scroller bar to trigger animation
 
-// build a scene
-var sceneOne = new ScrollMagic.Scene({
-    triggerElement: '#bellsbeach' 
 
+
+// bells beach controller
+var sceneOne = new ScrollMagic.Scene({
+    triggerElement: '#bellsbeach img',
+    duration: '100%',
+    triggerHook: 0.90,
 })
 .setClassToggle('#bellsbeach', 'fade-in') //add CSS class
 .addTo(controller);
 
 //lorne controller
 var sceneTwo = new ScrollMagic.Scene({
-    triggerElement: '#lorne' 
+    triggerElement: '#lorne img',
+    duration: '100%',
+    triggerHook: 0.90,
 })
 .setClassToggle('#lorne', 'fade-in') //add CSS class
 .addTo(controller);
 
 //koala controller
 var sceneThree = new ScrollMagic.Scene({
-    triggerElement: '#koala' 
+    triggerElement: '#koala img',
+    duration: '100%',
+    triggerHook: 0.90, 
 })
 .setClassToggle('#koala', 'fade-in') //add CSS class
 .addTo(controller);
 
 //otway controller
 var sceneFour = new ScrollMagic.Scene({
-    triggerElement: '#otway' 
+    triggerElement: '#otway img',
+    duration: '100%',
+    triggerHook: 0.90, 
 })
 .setClassToggle('#otway', 'fade-in') //add CSS class
+.addTo(controller);
+
+//apostles controller
+var sceneFive = new ScrollMagic.Scene({
+    triggerElement: '#apostles img',
+    duration: '100%',
+    triggerHook: 0.90, 
+})
+.setClassToggle('#apostles', 'fade-in') //add CSS class
+.addTo(controller);
+
+//loch controller
+var sceneSix = new ScrollMagic.Scene({
+    triggerElement: '#loch img',
+    duration: '100%',
+    triggerHook: 0.90, 
+})
+.setClassToggle('#loch', 'fade-in') //add CSS class
 .addTo(controller);
 
 
